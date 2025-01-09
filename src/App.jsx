fdc7b40139aeb8a251bc3bd62c1714dca5c9dc1d
jere daniel <jerejere660@gmail.com>
2025-01-09 03:18:56 -0300

    3er intento

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import Apidolarpage from '../pages/apidolar'
 function App() {
   return (
 
-    <Routes basename="/EnBiciweb">
-      <Route path='/' element={<Home></Home>}/>
+    <Routes>
+      <Route path='/EnBiciweb/' element={<Home></Home>}/>
       <Route path='/EnBiciweb/Apidolar' element={<Apidolarpage></Apidolarpage>}></Route>
     </Routes>
   )
